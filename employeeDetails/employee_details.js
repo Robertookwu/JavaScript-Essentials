7cec8421b502b0241e7223f7b615cbeb4a4e5562
Robertookwu <ookwurobert@gmail.com>
2024-12-20 12:54:21 -0500

    eight and half  commit

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,15 +1,15 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript'},
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
       //... More employee records can be added here
     ];
 function displayEmployees(){
     // Function to display all employees
     const totalEmployees = employees.map((employee, index) => {
-        return (`<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+        return `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`;
      });
-     document.getElementById('employeesDetails').innerHTML = totalEmployees;
+     document.getElementById('employeesDetails').innerHTML = totalEmployees.join('');
     
 }
  
@@ -36,3 +36,15 @@ function findEmployeeById(employeeId) {
 
       }
     }
+
+function findEmployeeBySpecialization(specialization){
+    const foundEmployee = employees.find(employee => employee.specialization === specialization);
+    if(foundEmployee)
+    {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}:${foundEmployee.name} - ${foundEmployee.department} - ${foundEmployee.specialization}</p>`;
+    }
+    else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this Specialization';
+    }
+    
+}
